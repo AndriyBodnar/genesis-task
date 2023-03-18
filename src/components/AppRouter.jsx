898a1f37692ed6fddcd30f26de2db2741e566f19
Andrii Bodnar <somniapurpura@gmail.com>
2023-03-18 21:09:33 +0200

    upd vite config

--- src/components/AppRouter.jsx
+++ src/components/AppRouter.jsx
@@ -7,16 +7,9 @@ import NotFound from "../pages/NotFound";
 const AppRouter = () => {
   return (
     <Routes>
-      <Route
-        path="/genesis-task"
-        element={<Navigate to="genesis-task/courses/" replace />}
-      />
-      <Route path="genesis-task/courses/" element={<Courses />} exact />
-      <Route
-        path="genesis-task/courses/:id/"
-        element={<CourseIdPage />}
-        exact
-      />
+      <Route path="/" element={<Navigate to="/courses/" replace />} />
+      <Route path="/courses/" element={<Courses />} exact />
+      <Route path="/courses/:id/" element={<CourseIdPage />} exact />
 
       <Route path="*" element={<NotFound />} />
     </Routes>
